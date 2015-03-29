8ae344a8c54666209da3eb1ef9b413b4cffbfa82
Thomas Bengtsson <thomasbengtssons@gmail.com>
2015-03-29 15:04:43 +0200

    Added header.php

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+		<div class="page" id="page1">
+			<div class="content container">
+				<h2>Heading goes here</h2>
+				<div class="row">
+					<p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis aperiam saepe assumenda, adipisci cumque vel dignissimos blanditiis obcaecati molestias qui amet sapiente eius, ab officia quis cum non, vitae quidem!</p>
+				</div> <!-- row END -->
+			</div> <!-- content END -->
+		</div> <!-- #page1 END --> 
