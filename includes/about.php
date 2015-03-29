3f214c5b9a442460cbfa460d3061b792bac663d7
Thomas Bengtsson <thomasbengtssons@gmail.com>
2015-03-29 23:25:00 +0200

    Added content to about.php

--- includes/about.php
+++ includes/about.php
@@ -1,8 +1,9 @@
 		<div class="page" id="about">
 			<div class="content container">
-				<h2>Heading</h2>
+				<i class="fa fa-mars"></i>
+				<h2>Hello there! <br> I'm Thomas Bengtsson</h2>
 				<div class="row">
-					<p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, numquam maiores quis! Iusto tenetur, expedita explicabo dolorem fugit ad, inventore voluptas at architecto minus corporis dignissimos nisi tempore doloribus laudantium.</p>
+					<p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, tempore nulla perferendis placeat. Natus voluptatem quod accusantium velit veritatis nemo autem explicabo quis dolor cumque illo exercitationem, deleniti nostrum, odit?</p>
 				</div> <!-- row END -->
 			</div> <!-- content END -->
 		</div> <!-- #page1 END --> 
