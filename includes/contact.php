82a2bf0f80b343ea390153595aaad9dd905c91de
Thomas Bengtsson <thomasbengtssons@gmail.com>
2015-03-29 15:49:20 +0200

    Initial commit for contact page

---
+++ includes/contact.php
@@ -0,0 +1,58 @@
+		    <div class="page" id="contact">
+		    	<div class="content container">
+		    		<h2>Kontakt</h2>
+		    		<div class="row">
+		    			<div class="col-md-6 contact-info">
+		    				<header>
+		    					<h3>Jeg er ledig til freelance jobs</h3>
+		    					<p>Skal vi arbejde sammen om dit næste projekt?</p>
+		    				</header>
+		    				<address>
+		    					<p>Thomas Bengtsson</p>
+		    					<p>Ellebækvej 56</p>
+		    					<p>2820 Gentofte</p>
+		    					<p>+45 60 71 46 42</p>
+		    				</address>
+		    				<div class="social-links">
+		    					<a href="#">
+		    						<span class="glyphicon glyphicon-heart"></span>
+		    					</a>
+		    					<a href="#">
+		    						<span class="glyphicon glyphicon glyphicon-fire"></span>
+		    					</a>
+		    					<a href="#">
+		    						<span class="glyphicon glyphicon glyphicon-magnet"></span>
+		    					</a>
+		    				</div> <!-- social-links -->
+		    				<div class="email">
+		    					<a href="#">Thomasbengtssons@gmail.com</a>
+		    				</div>
+		    			</div><!-- contact-info -->
+		    			<div class="col-md-6 contact-form">
+		    				<form class="form-horizontal">
+		    					<div class="form-group">
+		    						<div class="col-sm-12">
+		    							<input type="text" class="form-control" id="contact-name" placeholder="Navn">
+		    						</div>
+		    					</div>
+		    					<div class="form-group">
+		    						<div class="col-sm-12">
+		    							<input type="email" class="form-control" id="contact-email" placeholder="Email">
+		    						</div>
+		    					</div>
+		    					<div class="form-group">
+		    						<div class="col-sm-12">
+		    							<textarea id="contact-message" class="form-control" rows="3"></textarea>
+		    						</div>
+		    					</div>
+
+		    					<div class="form-group">
+		    						<div class="col-sm-12">
+		    							<button type="submit" class="btn btn-default">Send</button>
+		    						</div>
+		    					</div>
+		    				</form>
+		    			</div><!-- contact-form -->
+		    		</div><!-- row -->   
+		    	</div><!-- content container -->
+    		</div><!-- contact page -->
