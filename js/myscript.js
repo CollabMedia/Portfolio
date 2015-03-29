08395fb872744f1e86ccd60ff05835d97daf48f5
Bjarne Mølgaard Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2015-03-29 15:21:57 +0200

    Removed slide code

--- js/myscript.js
+++ js/myscript.js
@@ -3,22 +3,11 @@ $(function() {
   "use strict";
 
   var topoffset = 50; //variable for menu height
-  var slideqty = $('#featured .item').length;
+
   var wheight = $(window).height(); //get the height of the window
-  var randSlide = Math.floor(Math.random()*slideqty);
-
-  $('#featured .item').eq(randSlide).addClass('active');
-
 
   $('.fullheight').css('height', wheight); //set to window tallness  
 
-
-  //replace IMG inside carousels with a background image
-  $('#featured .item img').each(function() {
-    var imgSrc = $(this).attr('src');
-    $(this).parent().css({'background-image': 'url('+imgSrc+')'});
-    $(this).remove();
-  });
 
   //adjust height of .fullheight elements on window resize
   $(window).resize(function() {
@@ -69,20 +58,6 @@ $(function() {
       } //target.length
     } //click function
   }); //smooth scrolling
-
-  //Automatically generate carousel indicators
-  for (var i=0; i < slideqty; i++) {
-    var insertText = '<li data-target="#featured" data-slide-to="' + i + '"';
-    if (i === randSlide) {
-      insertText += ' class="active" ';
-    }
-    insertText += '></li>';
-    $('#featured ol').append(insertText);
-  }
-
-  $('.carousel').carousel({
-    pause: false
-  });
 
 
 $("#dynamicText").typed({
