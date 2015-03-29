1933f9bbe4692288b4942d90544f6977fa8fc18e
Thomas Bengtsson <thomasbengtssons@gmail.com>
2015-03-29 22:56:41 +0200

    Changed contact page
    Added
    Background image
    Overlay
    Misc. styles

--- includes/skills.php
+++ includes/skills.php
@@ -1,8 +1,8 @@
 		<div class="page" id="page2">
 			<div class="content container">
-				<h2>Heading goes here</h2>
+				<h2></h2>
 				<div class="row">
-					<p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis aperiam saepe assumenda, adipisci cumque vel dignissimos blanditiis obcaecati molestias qui amet sapiente eius, ab officia quis cum non, vitae quidem!</p>
+					<p class="col-md-12"></p>
 				</div> <!-- row END -->
 			</div> <!-- content END -->
 		</div> <!-- #page2 END -->  
