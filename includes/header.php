a725cbe6c2b62a1844da155c35b2f8b5033f27e8
Bjarne Mølgaard Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2015-03-29 15:32:23 +0200

    Ændrer featured til home

--- includes/header.php
+++ includes/header.php
@@ -8,31 +8,31 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#featured"><h1>BMGP <span class="subhead">media</span></h1></a>
+        <a class="navbar-brand" href="#home"><h1>BMGP <span class="subhead">media</span></h1></a>
       </div><!-- navbar-header -->
       <div class="collapse navbar-collapse" id="collapse">
         <ul class="nav navbar-nav navbar-right">
-          <li class="active"><a href="#featured">Home</a></li>
-          <li><a href="#mission">Mission</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#staff">Staff</a></li>
-          <li><a href="#testimonials">Testimonials</a></li>
+          <li class="active"><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contacts</a></li>
         </ul>        
       </div><!-- collapse navbar-collapse -->
     </div><!-- container -->
   </nav>
 
-  <div id="featured" class="fullheight">
+  <div id="home" class="fullheight">
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <div class="featuredText ">            
+          <div class="homeText ">            
             <p>Hello there</p>
             <p>I am a</p>
             <span id="dynamicText"></span><span class="typed-cursor"></span>
-          </div><!-- featured text -->
+          </div><!-- home text -->
         </div><!-- col-md-12 -->
       </div><!-- row -->
     </div><!-- container -->
-  </div><!-- featured -->
+  </div><!-- home -->
 </header>
