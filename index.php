533c241be3f86c4578f8db80ac29c023ba52b132
Thomas Bengtsson <thomasbengtssons@gmail.com>
2015-03-29 14:10:01 +0200

    Initial changes to index.php
    Added
    Starting template
    Link to style.css
    Link to myscript.js
    Link to Bootstrap
    Link to jQuery

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Portfolio</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/prefixfree.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+	<!-- Content goes here -->
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<script src="js/myscript.js"></script>
+</body>
+</html>
