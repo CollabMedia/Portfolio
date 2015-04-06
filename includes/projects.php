ce88966e0812367a5b84ac2d10133976cd0c3a07
Bjarne Mølgaard Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2015-04-06 16:20:39 +0200

    Projects with wrapper (beta)

--- includes/projects.php
+++ includes/projects.php
@@ -1,10 +1,68 @@
 <div class="page" id="projects">
-			<div class="content container">
-			
-				<h2>Projects</h2>
-				<div class="row">
-					<p class="intro-text">Row text</p>
-					<p>Normal text</p>
-				</div> <!-- row END -->
-			</div> <!-- content container END -->
-		</div> <!-- #page Projects END -->  
+	<div class="content container">
+		<header>
+			<h2>Projects</h2>
+			<img src="images/grey_line.png" class="line" alt="grey_line">
+			<h3>A collection of my developed projects</h3>
+
+		</header>
+	</div> <!-- content container END -->
+	<article>
+		<div class="container">
+			<div class="row">
+
+				<div class="col-md-4 col-xs-12">
+					<h4>Project 1</h4>
+					<h5>Sub heading</h5>
+					<img src="images/grey_line.png" class="line" alt="grey_line">
+					<p>This project is an administrator-controlled digital handbook system for guides and plugins that targets employees of a minor web company. I have developed the system from scratch in PHP and MySQL and used the Bootstrap framwork. The system has been securely developed to protected against attacks.</p><br>
+					<p>
+
+						To log in and use Logic Lead System:
+						● Employee: testuser@hotmail.com // Password: Testuser1
+						● Admin: testadmin@hotmail.com // Password: Testadmin1
+
+						Code can be found on Github:
+
+						https://github.com/Moelgaard85/LogicLeadSystem/
+					</p>
+				</div>
+				<div class="col-md-8 col-xs-12">
+					<div class="project-image">
+						<img src="images/logicleadsystem-770x440.jpg" class="img-responsive"></img>
+					</div>
+				</div>
+			</div>
+		</div>
+	</article>
+	<article>
+		<div class="container">
+			<div class="row">
+
+				<div class="col-md-4 col-xs-12">
+					<h4>Project 1</h4>
+					<h5>Sub heading</h5>
+					<img src="images/grey_line.png" class="line" alt="grey_line">
+					<p>This project is an administrator-controlled digital handbook system for guides and plugins that targets employees of a minor web company. I have developed the system from scratch in PHP and MySQL and used the Bootstrap framwork. The system has been securely developed to protected against attacks.</p><br>
+					<p>
+
+						To log in and use Logic Lead System:
+						● Employee: testuser@hotmail.com // Password: Testuser1
+						● Admin: testadmin@hotmail.com // Password: Testadmin1
+
+						Code can be found on Github:
+
+						https://github.com/Moelgaard85/LogicLeadSystem/
+					</p>
+				</div>
+				<div class="col-md-8 col-xs-12">
+					<div class="project-image">
+						<img src="images/mountain.jpg" class="img-responsive"></img>
+					</div>
+				</div>
+			</div>
+		</div>
+	</article>
+
+
+</div> <!-- #page Projects END -->  
