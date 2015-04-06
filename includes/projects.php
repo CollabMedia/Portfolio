2b41a45d03d3d2afb12ef46ae11b1cb5b35d7072
Bjarne Mølgaard Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2015-04-06 12:44:14 +0200

    Initializing projects.php

---
+++ includes/projects.php
@@ -0,0 +1,10 @@
+<div class="page" id="projects">
+			<div class="content container">
+			
+				<h2>Projects</h2>
+				<div class="row">
+					<p class="intro-text">Row text</p>
+					<p>Normal text</p>
+				</div> <!-- row END -->
+			</div> <!-- content container END -->
+		</div> <!-- #page Projects END -->  
