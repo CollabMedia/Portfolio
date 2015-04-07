93206e1f8846dd05d8d8ca5e068d9c9db4aac741
Bjarne Mølgaard Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2015-04-07 11:37:39 +0200

    Added carousel to projects_2

--- includes/projects.php
+++ includes/projects.php
@@ -34,7 +34,47 @@
 				<div class="col-md-8 col-xs-12">
 					<div class="project_content_right">
 						<div class="project-image">
-							<img src="images/logicleadsystem.jpg" class="img-responsive"></img>
+
+						<!-- Carousel 1 -->
+							<div id="Carousel1" class="carousel slide" data-ride="carousel">
+								<!-- Indicators -->
+								<ol class="carousel-indicators">
+									<li data-target="#Carousel1" data-slide-to="0" class="active"></li>
+									<li data-target="#Carousel1" data-slide-to="1"></li>
+									<li data-target="#Carousel1" data-slide-to="2"></li>
+									<li data-target="#Carousel1" data-slide-to="3"></li>
+								</ol>
+
+								<!-- Wrapper for slides -->
+								<div class="carousel-inner" role="listbox">
+									<div class="item active">
+										<img src="images/logicleadsystem.jpg" alt="logicleadsystem">
+									</div>
+
+									<div class="item">
+										<img src="images/logicleadsystem.jpg" alt="logicleadsystem">
+									</div>
+
+									<div class="item">
+										<img src="images/logicleadsystem.jpg" alt="logicleadsystem">
+									</div>
+
+									<div class="item">
+										<img src="images/logicleadsystem.jpg" alt="logicleadsystem">
+									</div>
+								</div>
+
+								<!-- Left and right controls -->
+								<!-- <a class="left carousel-control" href="#Carousel1" role="button" data-slide="prev">
+									<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+									<span class="sr-only">Previous</span>
+								</a>
+								<a class="right carousel-control" href="#Carousel1" role="button" data-slide="next">
+									<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+									<span class="sr-only">Next</span>
+								</a> -->
+							</div><!-- Carousel 1 End -->
+
 						</div>
 					</div>
 				</div><!-- col right image end -->
@@ -69,7 +109,49 @@
 				<div class="col-md-8 col-xs-12">
 					<div class="project_content_right">
 						<div class="project-image">
-							<img src="images/prattle.jpg" class="img-responsive"></img>
+
+
+						<!-- Carousel 2 -->
+							<div id="carousel2" class="carousel slide" data-ride="carousel">
+								<!-- Indicators -->
+								<ol class="carousel-indicators">
+									<li data-target="#carousel2" data-slide-to="0" class="active"></li>
+									<li data-target="#carousel2" data-slide-to="1"></li>
+									<li data-target="#carousel2" data-slide-to="2"></li>
+									<li data-target="#carousel2" data-slide-to="3"></li>
+								</ol>
+
+								<!-- Wrapper for slides -->
+								<div class="carousel-inner" role="listbox">
+									<div class="item active">
+										<img src="images/prattle.jpg" alt="prattle">
+									</div>
+
+									<div class="item">
+										<img src="images/prattle.jpg" alt="prattle">
+									</div>
+
+									<div class="item">
+										<img src="images/prattle.jpg" alt="prattle">
+									</div>
+
+									<div class="item">
+										<img src="images/prattle.jpg" alt="prattle">
+									</div>
+								</div>
+
+								<!-- Left and right controls -->
+								<!-- <a class="left carousel-control" href="#carousel2" role="button" data-slide="prev">
+									<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+									<span class="sr-only">Previous</span>
+								</a>
+								<a class="right carousel-control" href="#carousel2" role="button" data-slide="next">
+									<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+									<span class="sr-only">Next</span>
+								</a> -->
+							</div><!-- Carousel 2 End -->
+
+
 						</div>
 					</div>
 				</div><!-- col right image end -->
@@ -104,7 +186,47 @@
 				<div class="col-md-8 col-xs-12">
 					<div class="project_content_right">
 						<div class="project-image">
-							<img src="images/bookit.png" class="img-responsive"></img>
+
+							<!-- Carousel 3-->
+							<div id="carousel3" class="carousel slide" data-ride="carousel">
+								<!-- Indicators -->
+								<ol class="carousel-indicators">
+									<li data-target="#carousel3" data-slide-to="0" class="active"></li>
+									<li data-target="#carousel3" data-slide-to="1"></li>
+									<li data-target="#carousel3" data-slide-to="2"></li>
+									<li data-target="#carousel3" data-slide-to="3"></li>
+								</ol>
+
+								<!-- Wrapper for slides -->
+								<div class="carousel-inner" role="listbox">
+									<div class="item active">
+										<img src="images/bookit.png" alt="bookit">
+									</div>
+
+									<div class="item">
+										<img src="images/bookit.png" alt="bookit">
+									</div>
+
+									<div class="item">
+										<img src="images/bookit.png" alt="bookit">
+									</div>
+
+									<div class="item">
+										<img src="images/bookit.png" alt="bookit">
+									</div>
+								</div>
+
+								<!-- Left and right controls -->
+								<!-- <a class="left carousel-control" href="#carousel3" role="button" data-slide="prev">
+									<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+									<span class="sr-only">Previous</span>
+								</a>
+								<a class="right carousel-control" href="#carousel3" role="button" data-slide="next">
+									<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+									<span class="sr-only">Next</span>
+								</a> -->
+							</div><!-- Carousel 3 End -->
+
 						</div>
 					</div>
 				</div><!-- col right image end -->
