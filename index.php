ad07c822f0503912c59750e72fcd584970f43d32
Bjarne Mølgaard Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2015-04-07 18:21:11 +0200

    Cleaned up html markup

--- index.php
+++ index.php
@@ -1,32 +1,23 @@
-
-
-
 <!DOCTYPE html>
 <html lang="da">
-
 <head>
 	<meta charset="UTF-8">
 	<title>Portfolio</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/animate.css" type="text/css">
 	<link rel="stylesheet" href="css/style.css" type="text/css">
 	<script src="js/prefixfree.min.js"></script>
-
 	<meta name="description" content="Portfolio website for Bjarne Gerhardt-Pedersen">
 	<meta name="keywords" content="Portfolio Bjarne Gerhardt-Pedersen Web Developer">
 	<link rel="icon" type="image/ico" href="favicon.ico">
-
-
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-
 <body>
 	<header>
 		<!-- header section -->
 		<?php include_once 'includes/header.php' ?>
 	</header>
-
 	<div class="main">
 		<!-- about section -->
 		<?php include_once 'includes/about.php' ?>
@@ -41,10 +32,8 @@
 		<?php include_once 'includes/contact.php' ?>
 
 		<!-- footer -->
-		<?php include_once 'includes/footer.php' ?>
-		
+		<?php include_once 'includes/footer.php' ?>		
 	</div> <!-- .main END -->
-
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
